d4aa8f205059d1af8c329c939fdf0d851b8ef1a8
ashkrit@gmail.com <Crack@it12>
2023-11-11 12:01:40 +0800

    Add logs

--- codezen/codezen/src/main/java/com/codezen/plugin/GoogleThisAction.java
+++ codezen/codezen/src/main/java/com/codezen/plugin/GoogleThisAction.java
@@ -7,14 +7,19 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.Logger;
 
 public class GoogleThisAction extends AnAction {
+
+    private static final Logger LOG = Logger.getInstance(GoogleThisAction.class);
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
         final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
         final CaretModel caretModel = editor.getCaretModel();
         final String selectedText = caretModel.getCurrentCaret().getSelectedText();
+        LOG.info("Processing text ." + selectedText);
 
         BrowserUtil.browse(String.format("https://www.google.com/search?q=%s", selectedText));
 
